89e2f8413f38d628f569f66469cef09d565f41ac
kauvincent <jameskauvincent@gmail.com>
2026-01-04 00:38:15 +0300

    Updating the app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shoppage.component';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size}) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate();
+
   return (
-    <div
-      className={`${size} menu-item`}>
-      <div className='background-image'
-            style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+        onClick={() => navigate(linkUrl)}
       />
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
